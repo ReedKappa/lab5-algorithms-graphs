13421260085cb2df4d9fde919add4db4d8057a4f
ReedKat <goshaidze03@gmail.com>
2022-12-04 23:44:22 +0500

    First step, i guess...

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GraphsConsole
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,21 @@
+﻿namespace GraphsConsole;
+
+public class Vertex
+{
+    public int Number { get; set; }
+    public bool Oriented { get; set; }
+    public bool Visited { get; set; }
+
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Vertex(int number)
+    {
+        Number = number;
+    }
+
+    public override string ToString()
+    {
+        return $"{Number}, {Visited}";
+    }
+}
