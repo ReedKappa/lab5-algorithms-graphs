08562f0cce2c785d0389f7b061a594efc4631ddc
Amir <70907990+Amirka2@users.noreply.github.com>
2022-12-05 22:26:21 +0500

    проверка работы методов создания вершин и ребер, а также обхода в глубину

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -5,7 +5,19 @@
         public static void Main(string[] args)
         {
             Graph g = new Graph("table.csv");
-            g.DepthSearch();
+            //g.DepthSearch();
+            Graph gr = new Graph();
+            for (int i = 0; i < 5; i++)
+            {
+                gr.CreateVertex();
+            }
+            gr.CreateEdge(gr.Vertexes[0], gr.Vertexes[1]);
+            gr.CreateEdge(gr.Vertexes[0], gr.Vertexes[2]);
+            gr.CreateEdge(gr.Vertexes[0], gr.Vertexes[3]);
+            gr.CreateEdge(gr.Vertexes[2], gr.Vertexes[1]);
+            gr.CreateEdge(gr.Vertexes[2], gr.Vertexes[4]);
+
+            gr.DepthSearch();
         }
     }
 }
